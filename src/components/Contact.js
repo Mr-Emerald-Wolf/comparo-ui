8006d97c0f62adc7cbb47da0194c0a12232978d3
Mr-Emerald-Wolf <shivamsharma2002@gmail.com>
2022-04-15 15:20:29 +0530

    Contact Us done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,17 +4,21 @@ export class Contact extends Component {
   render() {
     return (
       <>
-      <section id="contact" className=" dark:bg-slate-800">
-                <div className="container p-4 mx-auto">
+      <section className="dark:bg-slate-800 h-screen">
+                <div className="p-4">
+                
 
-                    <div className="bg-gray-50 dark:bg-gray-900 py-6 mt-6 rounded shadow-md flex-col md:w-1/2 h-1/2 ">
+                    <div className="bg-gray-50 dark:bg-gray-900 py-6 mt-6 rounded shadow-md ">
+                    <h1 className="text-4xl md:text-6xl lg:text-9xl md:p-3 font-kalam font-bold dark:text-white text-black">
+                        Contact Us
+                    </h1>
                         <div className="px-6">
                             <h2 className="title-font font-semibold text-sky-900 dark:text-white tracking-widest md:text-2xl">
                                 ADDRESS
                             </h2>
                             <p className="mt-1 dark:text-sky-200 text-sky-700 md:text-xl">
-                                India <br />Delhi <br />
-                                Dwarka
+                                India <br/> 
+                                Hyderabad 
                             </p>
                         </div>
                         <div className="px-6 mt-4">
@@ -22,17 +26,26 @@ export class Contact extends Component {
                                 EMAIL
                             </h2>
                             <p className="dark:text-sky-200 text-sky-700 leading-relaxed md:text-xl">
-                                shivamsharma2002@gmail.com
+                            comparocommerce@gmail.com
                             </p>
 
                         </div>
                         <div className="px-6 mt-4">
                             <h2 className="title-font font-semibold text-sky-900 dark:text-white tracking-widest md:text-2xl">
-                                Github Profile
+                                Instagram
                             </h2>
-                            <a className="dark:text-sky-200 text-sky-700 leading-relaxed md:text-xl" href="https://github.com/Mr-Emerald-Wolf">
-                                Mr-Emerald-Wolf
+                            <a className="dark:text-sky-200 text-sky-700 leading-relaxed md:text-xl" href="https://www.instagram.com/comparo.ecommerce/">
+                               comparo.commerce
                             </a>
+
+                        </div>
+                        <div className="px-6 mt-4">
+                            <h2 className="title-font font-semibold text-sky-900 dark:text-white tracking-widest md:text-2xl">
+                               Telephone
+                            </h2>
+                            <p className="dark:text-sky-200 text-sky-700 leading-relaxed md:text-xl" >
+                            8555509495
+                            </p>
 
                         </div>
                     </div>
