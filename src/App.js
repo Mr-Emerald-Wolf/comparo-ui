6fee3997a6a9968beadb4458923d35bb6bd42d4a
Mr-Emerald-Wolf <shivamsharma2002@gmail.com>
2022-04-15 11:18:16 +0530

    Search bar created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import Landing from './components/Landing'
 
 function App() {
   return (
     <>
+    <Navbar />
+    <div className="flex flex-col h-screen">
+      <Landing />
+    </div>
     </>
   );
 }
