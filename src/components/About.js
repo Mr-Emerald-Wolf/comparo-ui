cce81e341912d9f686025f8ffab60fbc9ac1bd92
Mr-Emerald-Wolf <shivamsharma2002@gmail.com>
2022-04-16 16:54:17 +0530

    Name added

--- src/components/About.js
+++ src/components/About.js
@@ -32,7 +32,7 @@ export class About extends Component {
                                 Team Members
                             </h2>
                             <p className="dark:text-sky-200 text-sky-700 leading-relaxed md:text-xl">
-                                <br />Muhammad Imran Khan - Leader, Backend {"&"}  API Development <br />   Shivam - Front End {"&"} UI Designer <br /> Mudit Sultania - Content Writer <br />Suryansh Raj Pratap Singh - Marketing<br /> Riya Agrawal - Guide <br /> Pavan K - Project Manager {"&"} Bug Tester
+                                <br />Muhammad Imran Khan - Leader, Backend {"&"}  API Development <br />   Shivam Sharma - Front End {"&"} UI Designer <br /> Mudit Sultania - Content Writer <br />Suryansh Raj Pratap Singh - Marketing<br /> Riya Agrawal - Guide <br /> Pavan K - Project Manager {"&"} Bug Tester
                             </p>
 
                         </div>
